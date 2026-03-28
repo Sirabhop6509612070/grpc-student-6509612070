6cac1c2d1a0023da09f2541803fa0671371fb008
Sirabhop6509612070 <sirabhop.som@dome.tu.ac.th>
2026-03-28 11:56:42 +0700

    incomplete

---
+++ grpc-student/client/client.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"time"
+
+	pb "grpc-student/studentpb"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewStudentServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// res, err := client.GetStudent(ctx, &pb.StudentRequest{
+	// 	Id: 101,
+	// })
+	res, err := client.ListStudents(ctx, &pb.Empty{})
+	marshal, err := json.Marshal(res)
+
+	if err != nil {
+		log.Fatalf("Error calling GetStudent: %v", err)
+	}
+
+	// log.Printf("Student Info:")
+	// log.Printf("ID: %d", res.Id)
+	// log.Printf("Name: %s", res.Name)
+	// log.Printf("Major: %s", res.Major)
+	// log.Printf("Email: %s", res.Email)
+
+	log.Printf("Student list:")
+	log.Printf("%s", marshal)
+}
